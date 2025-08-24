78b548de5a69345d6298ac8e014d924b5d339f68
Rainier Estonio <155918061+ailujffshhh@users.noreply.github.com>
2025-08-24 13:21:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,3 +79,4 @@ if __name__ == "__main__":
     import uvicorn
     threading.Thread(target=lambda: uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000))), daemon=True).start()
     bot.run(DISCORD_TOKEN)
+ 
